e6a38b7919783792a5578b86db810fa4d8dfc699
Theodore Niu <theodore.niu@gmail.com>
2024-11-07 03:44:20 +0000

    support log

--- service.sh
+++ service.sh
@@ -28,5 +28,3 @@ sudo chown root:root /etc/systemd/system/graphrag_kit.service
 sudo systemctl stop graphrag_kit.service
 sudo systemctl enable graphrag_kit.service
 sudo systemctl start graphrag_kit.service
-
-# sudo journalctl -u graphrag_kit -f
