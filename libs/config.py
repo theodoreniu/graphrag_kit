578540c075a0049fad074344b631ec0b051a9a69
Theodore Niu <theodore.niu@gmail.com>
2024-12-01 10:10:18 +0000

    release 0.0.9

--- libs/config.py
+++ libs/config.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 load_dotenv()
 
-app_version = "0.0.8"
+app_version = "0.0.9"
 graphrag_version = "0.5.0"
 app_started_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 app_name = os.getenv('APP_NAME', 'graphrag')
